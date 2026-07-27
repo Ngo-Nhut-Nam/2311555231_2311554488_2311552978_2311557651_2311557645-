b08989d4ff044e65dd5598eb7ce7d64ffb3d8643
Khoa commit <khoahuynhkhoah12321@gmail.com>
2026-07-27 08:44:56 +0700

    khoa commit file - TopProductViwModel.cs, StatisticViewModel.cs, Index.cshtml, StatisticController.cs

---
+++ 2311555231_2311554488_2311552978_2311557651_2311557645-/Controllers/StatisticsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YourProject.Data;
+using _2311555231_2311554488_2311552978_2311557651_2311557645_.ViewModels;
+
+namespace _2311555231_2311554488_2311552978_2311557651_2311557645_.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StatisticsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var totalRevenue = await _context.Orders
+                .SumAsync(o => o.TotalAmount);
+
+            var topProducts = await _context.OrderDetails
+                .GroupBy(od => new
+                {
+                    od.ProductId,
+                    od.Product!.Name
+                })
+                .Select(g => new TopProductViewModel
+                {
+                    ProductName = g.Key.Name,
+                    TotalQuantitySold = g.Sum(od => od.Quantity)
+                })
+                .OrderByDescending(p => p.TotalQuantitySold)
+                .Take(3)
+                .ToListAsync();
+
+            return View(new StatisticsViewModel
+            {
+                TotalRevenue = totalRevenue,
+                TopProducts = topProducts
+            });
+        }
+    }
+}
